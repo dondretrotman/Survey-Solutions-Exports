de6750cfb796e0d8ca83148b753dfa8bc16931b4
Dondre <dondre_t@hotmail.com>
2024-08-26 10:29:16 -0400

    Update LFSExport.py
    Correctly handle url requests
    Check the url up to 3 times by default since the first time seems to result in a 503 error sometimes
    Cleaned up import statement, removed beep at the end

--- LFSExport.py
+++ LFSExport.py
@@ -11,8 +11,10 @@
 #TODO: Maybe use zipfile module instead of 7zip
 #
 
-import ssaw, os, winsound, sys, fileinput, time, json, urllib.request
+import ssaw, os, sys, fileinput, time, json
 import pandas as pd
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
 from ssaw import ExportApi
 from ssaw.models import ExportJob
 from datetime import datetime, date
@@ -56,6 +58,7 @@ finalfile = 'Weekly Updates\\'+today+'.txt'
 finalexcel = 'Weekly Updates\\CLFSS '+today+'.xlsx'
 numlines = 0
 quesid = data['questionnaire_identity']
+i = 3
 
 #For some reason the ExportJob function takes the questionnaire identity as questionnaire_id but the ExportApi function has questionnaire_identity
 #So I have to pass the arguments slightly differently for each function. Have no idea why it suddenly needed this distinction since at the time of this writing
@@ -82,18 +85,40 @@ print('Be patient! Some files can take a while to download')
 #Labourforce v18
 print('Getting LFS data V18 in tab format...')
 #check if the site is responding. 
-urllib.request.urlopen(data['url']).getcode()
-# there is no ready export, start a new job
-if generate == True and urllib.request.urlopen(data['url']).getcode() == 200:
-    job = ExportJob(**argsexportjob)
-    response = ExportApi(client, workspace=data['workspace']).start(job, wait=True, show_progress=True)
-    print("Downloading...")
-    response = ExportApi(client, workspace=data['workspace']).get(**argsexportapi, show_progress=True)
-elif generate == False and urllib.request.urlopen(data['url']).getcode() == 200:
-    print("Downloading...")
-    response = ExportApi(client, workspace=data['workspace']).get(**argsexportapi, show_progress=True)
-else:
-    sys.exit(f"Cannot connect to site, or invalid generate ({generate})")
+# httpresponse = urllib.request.urlopen(data['url']).getcode()
+# # there is no ready export, start a new job
+# if generate == True and urllib.request.urlopen(data['url']).getcode() == 200:
+    # job = ExportJob(**argsexportjob)
+    # response = ExportApi(client, workspace=data['workspace']).start(job, wait=True, show_progress=True)
+    # print("Downloading...")
+    # response = ExportApi(client, workspace=data['workspace']).get(**argsexportapi, show_progress=True)
+# elif generate == False and urllib.request.urlopen(data['url']).getcode() == 200:
+    # print("Downloading...")
+    # response = ExportApi(client, workspace=data['workspace']).get(**argsexportapi, show_progress=True)
+# else:
+    # sys.exit(f"Cannot connect to site, or invalid generate ({generate})")
+    
+#Use try/except statements to capture errors, loop 3 times
+while i > 0:
+    try:
+        httpresponse = urlopen(data['url'])
+    except HTTPError as e:
+        print('HTTP error code: ', e.code)
+        i = i - 1
+    except URLError as e:
+        print(f'Error reason: {e.reason}, trying {i} more time(s)')
+        i = i - 1
+    else:
+        if generate == True:
+            job = ExportJob(**argsexportjob)
+            response = ExportApi(client, workspace=data['workspace']).start(job, wait=True, show_progress=True)
+            print("Downloading...")
+            response = ExportApi(client, workspace=data['workspace']).get(**argsexportapi, show_progress=True)
+            break
+        elif generate == False:
+            response = ExportApi(client, workspace=data['workspace']).get(**argsexportapi, show_progress=True)
+            break
+
 move('CLFSS_18_Tabular_'+istatus+'_no-meta.zip', 'CLFSS_18_Tabular_'+istatus+'('+today+').zip')
 print('Done!\n')
 os.system(szip+r'CLFSS_18_Tabular_'+istatus+'('+today+').zip"')
@@ -134,5 +159,3 @@ endtime = datetime.now()
 print("Script ended at ", endtime)
 print("runtime is ", endtime-starttime)
 print("-------------------------------------------------------------------------------\n\n")
-#winsound.Beep(500,100)
-
